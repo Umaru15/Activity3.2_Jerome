33dd836e59e08692bbd8630de1fe54e1c40f7713
Jerome Maru Padre <jeromemaruperez15@gmail.com>
2023-11-10 13:16:30 +0800

    feat/insert-data

--- hero.sql
+++ hero.sql
@@ -66,3 +66,16 @@ CREATE TABLE IF NOT EXISTS public.HeroItem
     FOREIGN KEY (hero_id) REFERENCES Hero(hero_id),
     FOREIGN KEY (item_id) REFERENCES Item(item_id)
 );
+
+INSERT INTO Class (class_id, class_name, class_description)
+VALUES
+    (101, 'Warrior', 'A strong and melee-focused class'),
+    (102, 'Mage', 'A magic-wielding class'),
+    (103, 'Rogue', 'A stealthy and agile class'),
+    (104, 'Skilled Archers', 'Precise ranged attackers'),
+    (105, 'Range Archers', 'Skilled archers'),
+    (106, 'Assassin', 'Stealthy and high burst damage'),
+    (107, 'Fighter', 'Versatile combatants'),
+    (108, 'Support', 'Provide assistance and healing'),
+    (109, 'Tank', 'High durability and crowd control'),
+    (110, 'Wizard', 'Masters of arcane magic');
